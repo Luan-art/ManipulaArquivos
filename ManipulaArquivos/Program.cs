0893da52c9d482f29b7f043c6689361ad2234d0e
Luan-art <luanlf2003@gmail.com>
2024-05-07 15:57:46 -0300

    MelhoriasManipuilarVetor

--- ManipulaArquivos/Program.cs
+++ ManipulaArquivos/Program.cs
@@ -1,46 +1,94 @@
-﻿
-//string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-string path = @"C:\Dados\";
-string file = "arquivo.txt";
+﻿using ManipulaArquivos;
 
-if (!Directory.Exists(path))
+string path = @"C:\DadosEstoque\";
+string file = "productsTeste.txt";
+
+Product CreateProduct()
 {
-    Directory.CreateDirectory(path);
+    Console.WriteLine("informe um id:");
+    int id = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe a descrição do produto:");
+    string description = Console.ReadLine();
+
+    Console.WriteLine("Informe o preço do produto:");
+    double price = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe a quantidade disponível:");
+    int quantity = int.Parse(Console.ReadLine());
+
+    return new Product(id, description, price, quantity);
 }
 
-if (File.Exists(path + file)){
-
-    StreamReader sr = new StreamReader(path + file);
-    string line = sr.ReadToEnd();
-    Console.Clear();
-    Console.WriteLine(line);
-    sr.Close();
-
-    line += Console.ReadLine();
-    StreamWriter sw = new StreamWriter(path + file);
-    sw.WriteLine(line);
-    sw.Close();
-    
-    
-    Console.Clear();
-    
-    StreamReader sr2 = new StreamReader(path + file);
-
-    Console.WriteLine(sr2.ReadToEnd());
-
-    var retorno = File.ReadLines(path + file).ElementAt(2) ;
-
-    Console.WriteLine(retorno);
-
-    Console.ReadKey();
-
-    int item = 0;
-
-    foreach(string linha in File.ReadLines(path + file)){
-        item++;
-        if(item == 3)
-        {
-            Console.WriteLine(linha);
-        }
+void ShowAll(List<Product> recievedList)
+{
+    foreach (Product product in recievedList)
+    {
+        Console.WriteLine(product.ToString());
     }
 }
+
+bool CheckIfExists(string p, string f)
+{
+    if (!Directory.Exists(p))
+    {
+        Directory.CreateDirectory(p);
+    }
+    if (!File.Exists(p + f))
+    {
+        File.Create(p + f);
+    }
+
+    return true;
+}
+
+void SaveFile(List<Product> l, string p, string f)
+{
+    if (CheckIfExists(p, f))
+    {
+        StreamWriter sw = new(p + f);
+     
+        foreach (var item in l)
+        {
+
+            sw.WriteLine(item.ToString());
+        }
+
+        sw.Close();
+    }
+
+}
+
+
+List<Product> LoadFile(string p, string f)
+{
+    List<Product> l = new();
+    if (CheckIfExists(p, f))
+    {
+        string[] data;
+
+        foreach (var linha in File.ReadAllLines(p + f))
+        {
+            data = linha.Split(";");
+            l.Add(new Product(int.Parse(data[0]), data[1], double.Parse(data[2]), int.Parse(data[3])));
+        }
+    }
+    return l;
+}
+
+Console.WriteLine(">>> CADASTRO DE PRODUTOS<<<");
+
+//List<Product> products = new();
+
+//products.Add(CreateProduct());
+
+List<Product> products2 = new(LoadFile(path, file));
+
+products2.Add(CreateProduct());
+
+SaveFile(products2, path, file);
+
+ShowAll(products2);
+
+
+
