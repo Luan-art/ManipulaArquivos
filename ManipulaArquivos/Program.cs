7735a57d1e0137316e4f16ff06cbf51814688373
Luan-art <luanlf2003@gmail.com>
2024-05-07 13:42:24 -0300

    Add project files.

---
+++ ManipulaArquivos/Program.cs
@@ -0,0 +1,46 @@
+﻿
+//string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+string path = @"C:\Dados\";
+string file = "arquivo.txt";
+
+if (!Directory.Exists(path))
+{
+    Directory.CreateDirectory(path);
+}
+
+if (File.Exists(path + file)){
+
+    StreamReader sr = new StreamReader(path + file);
+    string line = sr.ReadToEnd();
+    Console.Clear();
+    Console.WriteLine(line);
+    sr.Close();
+
+    line += Console.ReadLine();
+    StreamWriter sw = new StreamWriter(path + file);
+    sw.WriteLine(line);
+    sw.Close();
+    
+    
+    Console.Clear();
+    
+    StreamReader sr2 = new StreamReader(path + file);
+
+    Console.WriteLine(sr2.ReadToEnd());
+
+    var retorno = File.ReadLines(path + file).ElementAt(2) ;
+
+    Console.WriteLine(retorno);
+
+    Console.ReadKey();
+
+    int item = 0;
+
+    foreach(string linha in File.ReadLines(path + file)){
+        item++;
+        if(item == 3)
+        {
+            Console.WriteLine(linha);
+        }
+    }
+}
